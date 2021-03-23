524058b1327d02b9ca85444af1af0bc35ccf7f73
Hisham164 <66801543+Hisham164@users.noreply.github.com>
2021-03-23 22:49:05 +0300

    post

--- app/src/main/java/com/example/publish_posts_firebase/adapters/AdapterNotification.java
+++ app/src/main/java/com/example/publish_posts_firebase/adapters/AdapterNotification.java
@@ -1,4 +1,61 @@
 package com.example.publish_posts_firebase.adapters;
 
-public class AdapterNotification {
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.publish_posts_firebase.R;
+import com.example.publish_posts_firebase.models.ModelNotification;
+
+import java.util.ArrayList;
+
+public class AdapterNotification extends RecyclerView.Adapter<AdapterNotification.HolderNotification> {
+
+    private Context context;
+    private ArrayList<ModelNotification> notificationsList;
+
+    public AdapterNotification(Context context, ArrayList<ModelNotification> notificationsList) {
+        this.context = context;
+        this.notificationsList = notificationsList;
+    }
+
+    @NonNull
+    @Override
+    public HolderNotification onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull HolderNotification holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    //holder class for views of row_notification.xml
+    class HolderNotification extends RecyclerView.ViewHolder{
+
+
+        //declare views
+        ImageView avatarIv;
+        TextView nameTv, notificationTv, timeTv;
+
+        public HolderNotification(@NonNull View itemView) {
+            super(itemView);
+
+            //init views
+            avatarIv = itemView.findViewById(R.id.avatarIv);
+            nameTv = itemView.findViewById(R.id.nameTv);
+            notificationTv = itemView.findViewById(R.id.notificationTv);
+            timeTv = itemView.findViewById(R.id.timeTv);
+        }
+    }
 }
