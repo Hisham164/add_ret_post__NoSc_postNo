d69e98cc1670d6426c88ebdf845f7034bd652b27
Hisham164 <66801543+Hisham164@users.noreply.github.com>
2021-03-25 16:35:50 +0300

    post

--- app/src/main/java/com/example/publish_posts_firebase/HomeFragment.java
+++ app/src/main/java/com/example/publish_posts_firebase/HomeFragment.java
@@ -3,23 +3,42 @@ package com.example.publish_posts_firebase;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.core.view.MenuItemCompat;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
+import android.widget.Toast;
 
+import com.example.publish_posts_firebase.adapters.Adapterpost;
+import com.example.publish_posts_firebase.models.Modelpost;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class HomeFragment extends Fragment {
 
-FirebaseAuth firebaseAuth;
-
+    FirebaseAuth firebaseAuth;
+    RecyclerView recyclerView;
+    List<Modelpost> postlist;
+    Adapterpost adapterpost;
 
     public HomeFragment() {
 
@@ -48,7 +67,88 @@ FirebaseAuth firebaseAuth;
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_home, container,false);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        //recyclerview and its properties
+        recyclerView = view.findViewById(R.id.recyclerview);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        //show newest post first, for this load from last
+        layoutManager.setStackFromEnd(true);
+        layoutManager.setReverseLayout(true);
+        //set layout to recyclerview
+        recyclerView.setLayoutManager(layoutManager);
+
+        //init post list
+        postlist = new ArrayList<>();
+
+        loadPost();
         return view;
+    }
+    private void loadPost() {
+
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Wirte your name of database");
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot datasnapshot) {
+
+                postlist.clear();
+                for (DataSnapshot ds: datasnapshot.getChildren()){
+                    Modelpost modelpost = ds.getValue(Modelpost.class);
+                    postlist.add(modelpost);
+
+                    //adapter
+                    adapterpost = new Adapterpost(getActivity(), postlist);
+                    //set adapter to recyclerview
+                    recyclerView.setAdapter(adapterpost);
+
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(getActivity(), ""+error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+
+    private void searchPost(final String searchQuery){
+
+
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Wirte your name of database");
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot datasnapshot) {
+
+                postlist.clear();
+                for (DataSnapshot ds: datasnapshot.getChildren()){
+                    Modelpost modelpost = ds.getValue(Modelpost.class);
+
+
+
+                    if (modelpost.getpTitle().toLowerCase().contains(searchQuery.toLowerCase()) ||
+                            modelpost.getpDescr().toLowerCase().contains(searchQuery.toLowerCase())){
+                        postlist.add(modelpost);
+                    }
+
+                    //adapter
+                    adapterpost = new Adapterpost(getActivity(), postlist);
+                    //set adapter to recyclerview
+                    recyclerView.setAdapter(adapterpost);
+
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(getActivity(), ""+error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
     private void checkUserStatus(){
@@ -64,25 +164,59 @@ FirebaseAuth firebaseAuth;
 
     public void onCreateOptionsMenu (Menu menu, MenuInflater inflater){
         inflater.inflate(R.menu.menu_main, menu);
+
+        MenuItem item = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(item);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                if (!TextUtils.isEmpty(query)){
+                    searchPost(query);
+                }else {
+
+                    loadPost();
+
+                }
+
+
+
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+
+                if (!TextUtils.isEmpty(newText)){
+                    searchPost(newText);
+                }else {
+
+                    loadPost();
+
+                }
+
+                return false;
+            }
+        });
         super.onCreateOptionsMenu(menu, inflater);
     }
 
-public boolean onOptionsItemSelected(MenuItem item){
+    public boolean onOptionsItemSelected(MenuItem item){
         //get item id
         int id = item.getItemId();
         if (id== R.id.action_logout){
             firebaseAuth.signOut();
             checkUserStatus();
         }
-    else if (id== R.id.action_add_post){
-       startActivity(new Intent(getActivity(), AddPostActivity.class));
-    }
-    else if (id==R.id.action_settings){
-        //go to setting activity
+        else if (id== R.id.action_add_post){
+            startActivity(new Intent(getActivity(), AddPostActivity.class));
+        }
+        else if (id==R.id.action_settings){
+            //go to setting activity
             startActivity(new Intent(getActivity(), SettingsActivity.class));
         }
 
         return super.onOptionsItemSelected(item);
-}
+    }
 
 }
